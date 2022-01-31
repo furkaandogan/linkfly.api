529359ebf400b0ac6d310b7c2036aea57830db79
Muhammet Furkan DOĞAN <dogan.furkan@msn.com>
2022-01-31 18:05:14 +0300

    NO-ISSUE / update

--- pkg/stringRandom/stringRandom.go
+++ pkg/stringRandom/stringRandom.go
@@ -1,13 +1,29 @@
 package stringrandom
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func RandString(n int) string {
-	b := make([]byte, n)
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func RandString(length int) string {
+	b := make([]byte, length)
 	for i := range b {
-		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+		b[i] = letterBytes[seededRand.Intn(len(letterBytes))]
 	}
 	return string(b)
+	// b := make([]byte, n)
+	// if _, err := rand.Read(b); err != nil {
+	// 	panic(err)
+	// }
+	// return fmt.Sprintf("%X", b)
+	// b := make([]byte, length)
+	// for i := range b {
+	// 	b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	// }
+	// return string(b)
 }
